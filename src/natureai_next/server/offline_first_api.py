6eda2c02904248c6cd45a20e335aa3939c710bca
Natuurgids.org <Snippit@chimaera-management.com>
2026-08-24 02:12:26 +0200

    Compose linked archive Operator health

--- src/natureai_next/server/offline_first_api.py
+++ src/natureai_next/server/offline_first_api.py
@@ -10,11 +10,13 @@ from natureai_next.server.linked_storage_api import (
     LinkedStorageRepository,
 )
 from natureai_next.server.linked_storage_browser_api import LinkedStorageBrowserFieldoraApi
+from natureai_next.server.linked_storage_operator_api import LinkedStorageOperatorApiMixin
 from natureai_next.server.linked_storage_sources_api import LinkedStorageSourcesApiMixin
 from natureai_next.server.offline_sync_api import OfflineSyncApiMixin, OfflineSyncRepository
 
 
 class OfflineFirstFieldoraApi(
+    LinkedStorageOperatorApiMixin,
     LinkedStorageSourcesApiMixin,
     LinkedStorageApiMixin,
     OfflineSyncApiMixin,

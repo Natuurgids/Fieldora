02d501fea4463aa19dbfee7fa8dd05a0f34d195f
Natuurgids.org <Snippit@chimaera-management.com>
2026-08-28 06:59:25 +0200

    WEB-048 wire governed server filters

--- src/natureai_next/server/offline_first_api.py
+++ src/natureai_next/server/offline_first_api.py
@@ -7,6 +7,7 @@ from collections.abc import Callable
 
 from natureai_next.server.administration_actions_api import AdministrationActionsApiMixin
 from natureai_next.server.facility_actions_api import FacilityActionsApiMixin
+from natureai_next.server.filtering import FilteringApiMixin
 from natureai_next.server.knowledge_parity_api import KnowledgeParityApiMixin
 from natureai_next.server.library_collections_api import LibraryCollectionsApiMixin
 from natureai_next.server.linked_storage_api import (
@@ -28,6 +29,7 @@ from natureai_next.server.visible_control_audit_api import VisibleControlAuditAp
 
 
 class OfflineFirstFieldoraApi(
+    FilteringApiMixin,
     PaginationApiMixin,
     VisibleControlAuditApiMixin,
     ObservationActionsApiMixin,

c1e42d52f492c6e2be39bcb6013e8d0811440536
Natuurgids.org <Snippit@chimaera-management.com>
2026-08-28 14:08:37 +0200

    Fix WEB-053 Ruff import canonicalization

--- src/natureai_next/server/postgres_web_indexes.py
+++ src/natureai_next/server/postgres_web_indexes.py
@@ -10,9 +10,7 @@ from __future__ import annotations
 
 from typing import Any
 
-from natureai_next.server import postgres_media
-from natureai_next.server import postgres_project_management
-from natureai_next.server import postgres_science
+from natureai_next.server import postgres_media, postgres_project_management, postgres_science
 
 
 _PROJECT_INDEXES = (

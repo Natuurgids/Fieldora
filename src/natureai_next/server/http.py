263cd9f23082e65d825fde40f1a74efbd14986cc
Natuurgids.org <Snippit@chimaera-management.com>
2026-08-31 06:31:52 +0200

    Finalize modular shell after managed web patches

--- src/natureai_next/server/http.py
+++ src/natureai_next/server/http.py
@@ -38,6 +38,7 @@ from natureai_next.server.linked_storage_operator_web import (
     patch_linked_storage_operator_web_response,
 )
 from natureai_next.server.linked_storage_web import patch_linked_storage_web_response
+from natureai_next.server.modular_shell_web import patch_modular_shell_response
 from natureai_next.server.navigation_web_compatibility import patch_navigation_web_response
 from natureai_next.server.offline_maps_web import patch_offline_maps_web_response
 from natureai_next.server.offline_models_web import patch_offline_models_web_response
@@ -135,6 +136,9 @@ def patch_managed_web_response(target: str, response):
         patch_offline_maps_web_response,
         patch_zero_trust_web_response,
         patch_aiadmin_zero_trust_response,
+        # Finalizer: migrated ownership must be evaluated after every legacy
+        # compatibility patch has had a chance to append its transitional JS.
+        patch_modular_shell_response,
     ):
         response = patch(target, response)
     return response

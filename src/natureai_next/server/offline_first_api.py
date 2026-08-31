add48f232b47650f00eb9e10c904d8fd1299882b
Natuurgids.org <Snippit@chimaera-management.com>
2026-08-31 08:22:49 +0200

    Compose Projects Core lifecycle module

--- src/natureai_next/server/offline_first_api.py
+++ src/natureai_next/server/offline_first_api.py
@@ -36,6 +36,9 @@ from natureai_next.server.project_idempotency import (
     wrap_project_management,
 )
 from natureai_next.server.project_idempotency_web import ProjectIdempotencyWebApiMixin
+from natureai_next.server.project_lifecycle_module_web import (
+    ProjectLifecycleModuleWebApiMixin,
+)
 from natureai_next.server.project_lifecycle_web import ProjectLifecycleWebApiMixin
 from natureai_next.server.project_runtime_web import ProjectRuntimeWebApiMixin
 from natureai_next.server.project_work_actions_module_web import (
@@ -48,6 +51,7 @@ from natureai_next.server.visible_control_audit_api import VisibleControlAuditAp
 
 class OfflineFirstFieldoraApi(
     ModularShellWebApiMixin,
+    ProjectLifecycleModuleWebApiMixin,
     ProjectWorkActionsModuleWebApiMixin,
     ProjectCoreModuleWebApiMixin,
     PortfolioModuleWebApiMixin,

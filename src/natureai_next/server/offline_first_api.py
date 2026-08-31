3b7ffb137672104c74e88dbd80864809ab1b7d83
Natuurgids.org <Snippit@chimaera-management.com>
2026-08-31 08:41:06 +0200

    Compose Project creation module

--- src/natureai_next/server/offline_first_api.py
+++ src/natureai_next/server/offline_first_api.py
@@ -30,6 +30,7 @@ from natureai_next.server.pagination_api import PaginationApiMixin
 from natureai_next.server.portfolio_module_web import PortfolioModuleWebApiMixin
 from natureai_next.server.postgres_web_indexes import ensure_managed_web_postgres_indexes
 from natureai_next.server.project_core_module_web import ProjectCoreModuleWebApiMixin
+from natureai_next.server.project_creation_module_web import ProjectCreationModuleWebApiMixin
 from natureai_next.server.project_hierarchy_web import ProjectHierarchyWebApiMixin
 from natureai_next.server.project_idempotency import (
     ProjectIdempotencyApiMixin,
@@ -51,6 +52,7 @@ from natureai_next.server.visible_control_audit_api import VisibleControlAuditAp
 
 class OfflineFirstFieldoraApi(
     ModularShellWebApiMixin,
+    ProjectCreationModuleWebApiMixin,
     ProjectLifecycleModuleWebApiMixin,
     ProjectWorkActionsModuleWebApiMixin,
     ProjectCoreModuleWebApiMixin,

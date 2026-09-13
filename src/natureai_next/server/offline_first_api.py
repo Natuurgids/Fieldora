0d00017f3343f00e23c263daadb40548f66e9867
Natuurgids.org <Snippit@chimaera-management.com>
2026-09-13 05:06:08 +0200

    Compose governed Excalidraw API

--- src/natureai_next/server/offline_first_api.py
+++ src/natureai_next/server/offline_first_api.py
@@ -18,6 +18,7 @@ from natureai_next.server.dossier_delete_api import DossierDeleteApiMixin
 from natureai_next.server.dossier_evidence_api import DossierEvidenceApiMixin
 from natureai_next.server.dossier_lifecycle_api import DossierLifecycleApiMixin
 from natureai_next.server.dossier_module_web import DossierModuleWebApiMixin
+from natureai_next.server.excalidraw_api import ExcalidrawApiMixin
 from natureai_next.server.facility_actions_api import FacilityActionsApiMixin
 from natureai_next.server.facility_module_runtime import (
     FacilityModuleCompositionApiMixin,
@@ -74,6 +75,7 @@ from natureai_next.server.visible_control_audit_api import VisibleControlAuditAp
 
 
 class OfflineFirstFieldoraApi(
+    ExcalidrawApiMixin,
     ModularShellWebApiMixin,
     BoundedUploadWebApiMixin,
     OperationsModuleCompositionApiMixin,
